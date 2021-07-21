b5d3cf52fcde3beb40dabb50e675e289f0e95402
Azard <iwant6108@126.com>
2021-07-21 21:11:44 +0800

    gen a maze

--- CrcyHead.h
+++ CrcyHead.h
@@ -3,11 +3,46 @@
 #define Inf 0xffffff
 #define up(i,d,u)   for(int i = (d);i <= (u);i++)
 #define down(i,u,d) for(int i = (u);i >= (d);i--)
+#define wall -1
+#define nothing 0
+#define d 6
+#define u 7
+#define l 8
+#define r 9
 using namespace std;
-
-class maze
+const int m=11,n=11;
+point st,ed;
+vector<blocks> myBlocks;
+int mx=1,my=1;
+int g[101][101];
+struct blocks
 {
-    public:
-        maze(void);
-        ~maze(void);
+    int row,col,dire;
+    blocks(int _row,int _col,int _dire)
+    {
+        row = _row;
+        col = _col;
+        dire = _dire;
+    }
 };
+struct point
+{
+    int x,y;
+};
+void inIt()
+{
+    memset(g,wall,sizeof(g));
+    g[1][1] = nothing;
+    st.x = st.y = 1;
+}
+void findBlock()
+{
+    if(mx+1 <= m && g[mx+1][my] == wall)
+        myBlocks.push_back(blocks(mx+1,my,d));
+    if(my+1 <= n && g[mx][my+1] == wall)
+        myBlocks.push_back(blocks(mx,my+1,r));
+    if(mx-1 <= m && g[mx-1][my] == wall)
+        myBlocks.push_back(blocks(mx-1,my,d));
+    if(my-1 <= n && g[mx][my-1] == wall)
+        myBlocks.push_back(blocks(mx,my-1,l));
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include "CrcyHead.h"
+//*Var*
+
+//*Var*
 int main()
 {
-    printf("Hello, World!");
+    inIt();
+    srand((unsigned)time(NULL));
+    findBlock();
+    while(myBlocks.size())
+    {
+        int blockSize = myBlocks.size();
+        int randnum = rand() % blockSize;
+        blocks selectBlock = myBlocks[randnum];
+        mx = selectBlock.row;
+        my = selectBlock.col;
+        switch (selectBlock.dire)
+        {
+        case d:{
+            mx++;
+            break;}
+        case r:{
+            my++;
+            break;}
+        case l:{
+            my--;
+            break;}
+        case u:{
+            mx--;
+            break;}
+        default:
+            break;
+        }
+        if(g[mx][my] == wall){
+            g[selectBlock.row][selectBlock.col] = g[mx][my] = nothing;
+            findBlock();
+        }
+        myBlocks.erase(myBlocks.begin()+randnum);
+    }
+    up(i,0,m+1){
+        up(j,0,n+1){
+            if(i==st.x && j==st.y)
+                printf("%c%c",0xa7,0xb0);
+            else if(g[i][j] == nothing)
+                printf("  ");
+            else    printf("%c%c",0xa8,0x80);
+        }
+        printf("\n");
+    }
     return 0;
 }
