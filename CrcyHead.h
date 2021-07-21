5819b0eb1f07b3b426b935e1b739449fb436098f
Azard <iwant6108@126.com>
2021-07-21 20:32:11 +0800

    Add some head unit

---
+++ CrcyHead.h
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#define Inf 0xffffff
+#define up(i,d,u)   for(int i = (d);i <= (u);i++)
+#define down(i,u,d) for(int i = (u);i >= (d);i--)
+using namespace std;
+
+class maze
+{
+    public:
+        maze(void);
+        ~maze(void);
+};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include "CrcyHead.h"
 int main()
 {
     printf("Hello, World!");
